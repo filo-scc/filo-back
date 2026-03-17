b0dedacb7a1e7dd6d56bb853f345932f0d5db28e
Gheyson <gheysonmelo@hotmail.com>
2026-03-17 08:54:49 -0300

    add: auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FabricoModule } from './fabrico/fabrico.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [FabricoModule],
+  imports: [FabricoModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
