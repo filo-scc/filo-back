6d12dd42afa47c1c10588015687147bcd069d30f
Gheyson <gheysonmelo@hotmail.com>
2026-05-04 18:23:01 -0300

    refactor: cep and responsavel field at migrations

--- prisma/migrations/20260503224720_add_grade_module/migration.sql
+++ prisma/migrations/20260503224720_add_grade_module/migration.sql
@@ -1,12 +1,9 @@
 /*
   Warnings:
 
-  - You are about to drop the column `cep` on the `enderecos` table. All the data in the column will be lost.
   - Added the required column `grade_versao_id` to the `fichas-tecnicas` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "enderecos" DROP COLUMN "cep";
 
 -- AlterTable
 ALTER TABLE "fichas-tecnicas" ADD COLUMN     "grade_versao_id" INTEGER NOT NULL;
