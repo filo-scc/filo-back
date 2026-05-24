416e6fde6e5675adf63d6fc22d20f39692982e3a
Erico <erico.chen@hotmail.com>
2026-05-24 16:32:21 -0300

    Merge branch Master into feature/cor

--- src/cor/dto/create-cor.dto.ts
+++ src/cor/dto/create-cor.dto.ts
@@ -1,5 +1,10 @@
 import { Type } from "class-transformer";
-import { IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
+import { IsNumber, IsOptional, IsString, MaxLength, IsEnum } from "class-validator";
+
+export enum TipoCorEnum {
+    LISA = "LISA",
+    ESTAMPA = "ESTAMPA",
+}
 
 export class CreateCorDto {
     @IsString()
@@ -13,4 +18,15 @@ export class CreateCorDto {
     @Type(() => Number)
     @IsNumber()
     fabrico_id: number;
+
+    @IsString()
+    @IsOptional()
+    foto?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsEnum(TipoCorEnum, {
+        message: "O tipo de cor deve ser: LISA ou ESTAMPA",
+    })
+    tipo?: TipoCorEnum;
 }
