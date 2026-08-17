9736dd0e23715f444916d3d6e62cb145b6eebb93
Joaoamlp <joaoamlp@gmail.com>
2026-08-17 18:48:53 -0300

    Revert "add: new attributes to FT"
    This reverts commit 3a2bef9476d2d0811ce1c0a290974ca277c2f3c4.

--- prisma/migrations/20260817203431_add_new_attributes_to_ft/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "fichas-tecnicas" ADD COLUMN     "defeitos_costura" INTEGER DEFAULT 0,
-ADD COLUMN     "defeitos_tecido" INTEGER DEFAULT 0,
-ADD COLUMN     "retiratas" INTEGER DEFAULT 0,
-ADD COLUMN     "sobras" INTEGER DEFAULT 0;
--- src/ficha-tecnica/dto/create-ficha-tecnica.dto.ts
+++ src/ficha-tecnica/dto/create-ficha-tecnica.dto.ts
@@ -20,16 +20,4 @@ export class CreateFichaTecnicaDto {
 
     @IsInt()
     quantidade: number;
-    
-    @IsInt()
-    defeitos_costura: number;
-
-    @IsInt()
-    defeitos_tecido: number;
-
-    @IsInt()
-    retiratas: number;
-    
-    @IsInt()
-    sobras: number;
 }
