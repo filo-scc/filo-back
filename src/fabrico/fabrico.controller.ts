207c00e4b88378ea01047cb59bce7daa2bce5ed2
Gheyson <gheysonmelo@hotmail.com>
2026-08-28 15:02:51 -0300

    refactor: auth roles at fabrico controller

--- src/fabrico/fabrico.controller.ts
+++ src/fabrico/fabrico.controller.ts
@@ -17,30 +17,34 @@ import { Roles } from "src/common/decorators/roles.decorator";
 
 @Controller("fabricos")
 @UseGuards(JwtAuthGuard, RolesGuard)
-@Roles("ADMIN")
 export class FabricoController {
     constructor(private readonly fabricoService: FabricoService) {}
 
+    @Roles("ADMIN")
     @Post()
     create(@Body() data: CreateFabricoDto) {
         return this.fabricoService.create(data);
     }
 
+    @Roles("ADMIN")
     @Get()
     getAll() {
         return this.fabricoService.getAll();
     }
 
+    @Roles("ADMIN", "PROPRIETARIO", "GERENTE")
     @Get(":id")
     getById(@Param("id", ParseIntPipe) id: number) {
         return this.fabricoService.getById(id);
     }
 
+    @Roles("ADMIN")
     @Put(":id")
     update(@Param("id", ParseIntPipe) id: number, @Body() data: CreateFabricoDto) {
         return this.fabricoService.update(id, data);
     }
 
+    @Roles("ADMIN")
     @Delete(":id")
     delete(@Param("id", ParseIntPipe) id: number) {
         return this.fabricoService.delete(id);
